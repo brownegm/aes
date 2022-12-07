5aaf317e4b989dbe39b00bee653b15765b0293f8
brownegm <32085030+brownegm@users.noreply.github.com>
2022-12-07 08:57:01 -0800

    initial commit

---
+++ R/color.palletes.R
@@ -0,0 +1,61 @@
+
+#' Color palletes to make the heart sing lol
+#'  Herein lies the warm tones to make your plot pop.
+#'    "@color.pallete" cinema on instagram is the main source of the movie specific palletes
+#'  To see the individual colors and plot them , see the R script in /exec
+
+#' bluepurp
+#' @format a list of five bluish purple colors
+#' \describe{
+#' }
+ "bluepurp"
+
+#' earthy.blueyellow
+#' 
+#' @format a list of five earthy bluish yellow colors 
+#' \describe{
+#' }
+ "earthy.blueyellow"
+
+ #' earthy.forest
+ #' 
+ #' @format a list of 5 earthy greens
+ #' \describe{
+ #' }
+ "earthy.forest"
+ 
+ #' Get Out
+ #' 
+ #'@format a list of 7 colors based on the movie get out 
+ #'\describe{
+ #' }
+ "get.out"
+ 
+ #'Handmaids Tale
+ #'
+ #'@format a list of 7 colors based on the show handmaids tale
+ #'\describe{
+ #' }
+ "handmaids"
+ 
+ #'Randos
+ #'
+ #'@format a list of 10 random earthy colors that I like
+ #'\describe{
+ #' }
+ "rando"
+ 
+ #'Sorry to bother you
+ #'
+ #'@format a list of 7 colors based on the movie Sorry to bother you
+ #'\describe{
+ #' }
+ "sorry2botheryou"
+ 
+ #' Taxi Driver
+ #' 
+ #'@format a list of 6 colors based on the movie Taxi Driver
+ #'\describe{
+ #' }
+ "taxi.driver"
+ 
---
+++ R/plotaxes.R
@@ -0,0 +1,50 @@
+#' Plot axis creation
+#' @description The purpose of this function is to create  even plot ticks at the minimum, middle and max values of the plot. 
+#' 
+#' @param dat A dataframe with variable to create axis information for 
+#' @param colName Name of variable do you need to find intervals for (make sure there are quotations around the colnames )
+#' @param round Number of digits to round axis values to. Default is 2
+#'
+#' @return A list of three values 
+#' 
+#' @export
+#' 
+#' @import dplyr
+#' 
+#' @family aesthetics
+#' @keywords plotting aesthetics
+#' 
+#' @examples
+#' #create three evenly spaced axis ticks for weight column on mtcars dataset
+#' axis_ticks<-plotaxis(dat=mtcars,colName = "wt")
+#' 
+#' #Slightly more complicated 
+#' #create three evenly spaced axis ticks for airquality dataset
+#' months<-unique(airquality$Month)#unique list of months of year sampled
+#'
+#' dats<-lapply(months, function (month) filter(airquality,Month==month))#create list of datasets, one for each month
+#' axis_ticks<-lapply(dats, function(x) plotaxis(dat = x, colName = "Ozone"))#create set of evenly spaced axis tick values for all datasets.
+#' 
+#' axis_ticks 
+
+plotaxis<-function(dat,
+                   colName,
+                   round=2){
+
+  dd<-dat%>% 
+    ungroup%>%
+    select(!!sym(colName))
+  
+  #determining minimum, middle, and max of the particular variable plotted....default is to round to 2 digits. 
+  min<-min(dd, na.rm=T)%>%round(digits=round)# calculate the minimum value of vector of values dd
+  mid<-mean(c(min(dd, na.rm =T),max(dd, na.rm =T)))%>%round(digits=round)# calculate the middle value of vector of values dd
+  max<-max(dd, na.rm =T)%>%round(digits=round)# calculate the max value of vector of values dd
+  
+  #output
+  axes<-c(min, mid, max)#save each of those previously calculated values. 
+  
+  return(axes)
+}
+
+
+
