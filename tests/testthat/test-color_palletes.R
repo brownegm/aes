d8739720578d1dd1590f38c53e1d68d523ed0385
brownegm <32085030+brownegm@users.noreply.github.com>
2025-09-04 08:41:01 -0700

    added the color scale

--- tests/testthat/test-color_palletes.R
+++ tests/testthat/test-color_palletes.R
@@ -1,3 +1,16 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("color palletes function", {
+  library(ggplot2)
+  
+  airquality |> 
+    ggplot2::ggplot(aes(x = Ozone, y = Temp, color = Day)) +
+    geom_point(size = 3) +
+    aes::scale_color_bean(discrete = FALSE) +
+    aes::theme_base()
+  
+  airquality |> 
+    ggplot2::ggplot(aes(x = Ozone, y = Temp, fill = Day)) +
+    geom_point(shape = 21, size = 3) +
+    aes::scale_fill_bean(palette = "sorry2botheryou", discrete = FALSE) +
+    aes::theme_base()
+
 })
